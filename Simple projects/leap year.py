9bedf1ff19ebb56dd3a30f81a7139ea19d5152ac
Mine <devildog.kk@gmail.com>
2024-11-05 15:46:33 +0700

    hh

---
+++ Simple projects/leap year.py
@@ -0,0 +1,11 @@
+def checkyear(year):
+    if (year%4==0 and year%100 !=0 )or year %400==0:
+        print("It's Leap Year")
+    else :
+        (print("Not a leap year"))
+        
+    
+
+#mock data
+year=int(input("Input to check Check Leap Year"))
+checkyear(year)
---
+++ Simple projects/leap_year.js
@@ -0,0 +1,11 @@
+//mock data 
+const year=2000
+
+//implement
+function checkLeapYear(year) {
+
+
+}
+
+//test
+console.log();
